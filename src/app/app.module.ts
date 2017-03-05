03ef9745c4a79c4b76489f4f287fbe3e42c9cee2
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-03-05 11:03:58 -0800

    Added RecipesComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
