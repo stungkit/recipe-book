67e9062b93a036a1358e7ce62ed4b6181c11baa4
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-03-18 15:45:58 -0700

    Completed Adding & Editing Mode.

--- src/app/recipes/shopping-list/shopping-list-add.component.ts
+++ src/app/recipes/shopping-list/shopping-list-add.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-add.component.css']
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd = true;
 
   constructor() { }
 
