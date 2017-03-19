d2b83953f4bd8b4355654db5a4eee5fea22abcca
Smati Tungkitkancharoen <stungkit@gmail.com>
2017-03-18 17:09:11 -0700

    Selecting Items & Property Binding.

--- src/app/recipes/shopping-list/shopping-list-add.component.ts
+++ src/app/recipes/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 import { Ingredient } from '../../ingredient';
 import { ShoppingListService } from './shopping-list.service';
@@ -8,13 +8,24 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list-add.component.html',
   styleUrls: ['./shopping-list-add.component.css']
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnInit, OnChanges {
+
+  @Input()
   item: Ingredient;
+
   isAdd = true;
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes): void {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+    } else {
+      this.isAdd = false;
+    }
   }
 
   onSubmit(ingredient: Ingredient) {
--- src/app/recipes/shopping-list/shopping-list.component.ts
+++ src/app/recipes/shopping-list/shopping-list.component.ts
@@ -9,11 +9,16 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   items: Ingredient[] = [];
+  selectedItem: Ingredient = null;
 
   constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
     this.items = this.sls.getItems();
+  }
+
+  onSelectItem(item: Ingredient) {
+    this.selectedItem = item;
   }
 
 }
